5bc162ef6fddb77b7fd7bd33fb0713320f71e501
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2017-02-23 22:00:44 +0000

    Extract getTokenConfig as a parameter.

--- matrix.js
+++ matrix.js
@@ -1,32 +1,18 @@
 // @flow
 
-import type {Matrix} from './lib/matrix/matrix'
-import type {TokenConfig} from './lib/matrix/token-config'
 import getFile from './lib/matrix/file'
-import clear from './lib/clear'
+import runner from './lib/matrix/runner'
 import render from './lib/matrix/render'
-import sanitize from './lib/matrix/sanitize'
 import getTokenConfig from './lib/matrix/get-token-config'
-import createMatrix from './lib/matrix/create-matrix'
-import calculateProbabilities from './lib/matrix/calculate-probabilities'
-import getWord from './lib/matrix/get-word'
-import {Observable} from 'rx'
+import clear from './lib/clear'
 
 clear()
 
 const filePath = __dirname + '/dict.txt'
 const file$ = getFile(filePath)
 
-file$
-    .map(sanitize)
-    .map(getTokenConfig)
-    .flatMap((tokenConfig: TokenConfig) => {
-        return createMatrix(tokenConfig)
-                .map(calculateProbabilities)
-                .flatMap( (matrix: Matrix) => {
-                    return Observable.range(0, 20)
-                        .flatMap( _ => getWord(tokenConfig, matrix) )
-                        .reduce( (acc, val)=> acc.concat(val), [] )
-                })
-    })
-    .subscribe(render)
+runner(
+    file$,
+    getTokenConfig
+)
+.subscribe(render)
